98a286362454c0cab8748ac79d7266c9a69d9034
Alejandro Verde <ale.verde234@gmail.com>
2025-10-23 00:11:54 -0400

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Bienvenidos a nuestra API REST");
-});
+//app.use((req, res, next) =>{
+    //res.json({messge: "Hola middleware"})
+//});
+
+//app.get('/', (req, res) =>{
+   // res.send("Bienvenidos a nuestra API REST");
+//});
+
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound)
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`escuchando puerto:${PORT}`));
